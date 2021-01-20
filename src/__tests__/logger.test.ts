9f2a8e18269451b546b0286fa6f57658a1c2e40f
Vadim Pospelov <vadim.pospelov@itrexgroup.com>
2021-01-20 21:04:56 +0200

    Setup Package

---
+++ src/__tests__/logger.test.ts
@@ -0,0 +1,10 @@
+import { Logger } from '../index';
+
+const logger = {
+    debug: 'Somme debug message', // TODO: mock the console log
+}
+
+test('Logger to log', () => {
+    const logger = Logger.create(['tag1', 'tag2'], 'local');
+    expect(logger.debug).toBe('Somme debug message');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-export * from './implementations/logger.service';
+import { LambdaLog } from 'lambda-log';
+import { ILoggerService } from './logger.interface';
+
+export class Logger implements ILoggerService {
+    private lambdaLogger: any;
+
+    private stage: string;
+
+    constructor(tags: any[], env: string) {
+        this.stage = env;
+
+        this.lambdaLogger = new LambdaLog();
+        this.lambdaLogger.options.tags.push(...tags);
+    }
+
+    static create(tags: any[], env: string) {
+        return new Logger(tags, env);
+    }
+
+    log(message: string) {
+        if (this.stage === 'local') return console.log(message);
+        return this.lambdaLogger.info(message);
+    }
+
+    warn(message: string | object) {
+        if (this.stage === 'local') return console.log(message);
+        return this.lambdaLogger.warn(message);
+    }
+
+    error(message: string | object) {
+        if (this.stage === 'local') return console.log(message);
+        return this.lambdaLogger.error(message);
+    }
+
+    debug(message: string | object) {
+        if (this.stage === 'local') return console.log(message);
+        return this.lambdaLogger.debug(message);
+    }
+
+    verbose(message: string | object) {
+        if (this.stage === 'local') return console.log(message);
+        return this.lambdaLogger.debug(message);
+    }
+}
