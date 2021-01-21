ff667bda07245d73c1c207ff7b1f4ed95d86e8ab
Vadim Pospelov <vadim.pospelov@itrexgroup.com>
2021-01-21 23:29:31 +0200

    fit

--- src/logger.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ILoggerService {
-    log(message: string, tags?: string[]): void;
-    warn(message: string, tags?: string[]): void;
-    debug(message: string, tags?: string[]): void;
-    error(message: string, tags?: string[]): void;
-    verbose(message: string, tags?: string[]): void;
-}
